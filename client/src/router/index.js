c4916cc5fca6d2555244cf79af55e55a765f196b
= <=>
2023-12-22 23:21:33 +0100

    Update: home doesnt require auth

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,9 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-      meta: {
-        requiresAuth: true
-      }
     },
     {
       path: '/login',
