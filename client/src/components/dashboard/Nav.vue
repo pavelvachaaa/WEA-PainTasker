52247c49c0f1b58733ca70db229e839d6c15f052
= <=>
2023-12-24 21:29:42 +0100

    .

--- client/src/components/dashboard/Nav.vue
+++ client/src/components/dashboard/Nav.vue
@@ -13,7 +13,7 @@ const user = computed(() => store.getters["auth/getUser"])
 <template>
     <header class="antialiased">
         <nav class="bg-white border-gray-200 px-4 lg:px-10 py-2.5 dark:bg-gray-800">
-            <div class="flex justify-between items-center">
+            <div class="flex flex-wrap justify-between items-center">
                 <div class="flex justify-start items-center">
 
                     <a href="/dashboard" class="flex mr-4">
